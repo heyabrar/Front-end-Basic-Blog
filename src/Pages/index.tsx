62c7890c9f66c4a90f4fc392ec542976a75db0ff
Abrar <abrar@ineuron.ai>
2023-10-13 21:13:39 +0530

    add: Basic Home Added

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import { homeHeroImage } from "../utils/blogs.constants";
 
-type Props = {};
-
-const Home = (props: Props) => {
-  return <div>Home</div>;
+const Home = () => {
+  return (
+    <div>
+      <div className="border w-[60%] mx-auto mt-6 rounded-md shadow-md">
+        <img src={homeHeroImage} alt="HeroImage" />
+      </div>
+      <div className="mt-5 flex justify-center gap-x-4 ">
+        <button
+          className="px-4 py-2 w-[150px] rounded-md bg-purple-100 hover:bg-white"
+          onClick={() => {
+            window.location.href = "/blogs";
+          }}
+        >
+          View Blogs
+        </button>
+        <button
+          className="px-4 py-2 w-[150px] rounded-md bg-purple-100 hover:bg-white"
+          onClick={() => {
+            window.location.href = "/create-blog";
+          }}
+        >
+          Create Blog
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
